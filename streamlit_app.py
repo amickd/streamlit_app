57d9d06f3db87236108662da7bdf220e628075a2
amickd <amickwilliam@gmail.com>
2023-04-13 21:07:50 -0400

    Update streamlit_app.py
    fixed an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,9 @@ streamlit.text('🥑🍞 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
     
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 
 #adding multiselect for UX
@@ -20,8 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.header("Fruityvice Fruit Advice!")
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
