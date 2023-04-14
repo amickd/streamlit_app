03c6386b7b342d5d90aabea16c473992f8decd8c
amickd <amickwilliam@gmail.com>
2023-04-13 21:10:35 -0400

    Update streamlit_app.py
    added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# take request and conform to flat table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#write table to the UI
+streamlit.dataframe(fruityvice_normalized)
