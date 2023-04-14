87eb9ffe08ddf8c32f2e55659dc3a958221d161b
amickd <amickwilliam@gmail.com>
2023-04-13 22:14:17 -0400

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list():
 	with my_cnx.cursor() as my_cur:
 		my_cur.execute("select * from fruit_load_list")
-		return my_cure.fetchall()
+		return my_cur.fetchall()
 
 
 
