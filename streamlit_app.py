e804c48ae4ef563a3723da5ec250e79757490296
amickd <amickwilliam@gmail.com>
2023-04-13 21:05:53 -0400

    Update streamlit_app.py
    added fruityvice api calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import pandas
 import streamlit
+import requests
+
 
 streamlit.title("My Parents Diner")
 streamlit.header('🥣 Breakfast Menu')
@@ -9,8 +11,8 @@ streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
     
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 #adding multiselect for UX
@@ -18,4 +20,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
